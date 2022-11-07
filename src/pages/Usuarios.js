bc6e168bf8ee917358911bfd9a85105120059b89
abraham-cardenas <abrahamcar098@gmail.com>
2022-11-07 10:06:45 -0500

    creacion de vehiculos

--- src/pages/Usuarios.js
+++ src/pages/Usuarios.js
@@ -1,18 +1,10 @@
-import React, { useEffect } from 'react';
 import { Table } from 'reactstrap';
-import { useVehiclesStore } from '../hooks/useVehiclesStore'
+
 
 
 export const Usuarios= () => {
 
-  const { startLoadingVehicles, vehiculos } = useVehiclesStore();
 
-
-  useEffect(() => {
-    startLoadingVehicles();
-
-
-  }, [])
 
   return (
     <div className='Container w-100 p-3'>
@@ -27,23 +19,6 @@ export const Usuarios= () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            {
-              vehiculos.map(vehiculo => (
-                <>
-                  <th scope="row">{vehiculo.Id}</th>
-                  <td>{vehiculo.Nombres}</td>
-                  <td>{vehiculo.Apellidos}</td>
-                  <td>{vehiculo.clave}</td>
-                  <td>{vehiculo.IdtipoUsuario}</td>
-                </>
-
-              ))
-            }
-
-
-          </tr>
-
         </tbody>
       </Table>
     </div>
