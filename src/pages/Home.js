b718c55798c7dc6901af6ca9c341fed90b995c18
abraham-cardenas <abrahamcar098@gmail.com>
2022-11-13 22:09:20 -0500

    fron-end solver

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,27 +7,26 @@ export const Home = () => {
       <form class="row g-3" name='rest'>
         <h3 className='p-1 m-1'>
           FUNCION OBJETIVO...........
-        </h3>
-        <p className='m-1 p-1'> F(x,y)</p>
-        <div class="row g-3">
-          <div class="col">
-            <p class="m-1">x</p>
-            <Input type='text' name='fx' size={3}></Input>
-          </div>
-          <div class="col">
-            <p class="col-md-1 m-1 ">y</p>
-            <Input type='text' name='fy' size={3}>y</Input>
+        </h3> 
+        <div class="row form-group g-3">
+          <div class="col-md-1 form-group">
+          <p className='m-1 p-1'> F(x,y)=</p>
+            <Input type='text' name='fx' size={3} placeholder="x"></Input>
+            +
+            <Input type='text' name='fy' size={3} placeholder="y"></Input>
           </div>
         </div>
         <p1>RESTRICCIONES</p1>
         <div class="row g-3">
-          <div class="col m-1">
-          <Input type='text' value={0} size={3}>x+</Input>
+          <div class="col-md-2 m-1">
+          <p class="m-0 p-0 text-center">Van N300</p>
+          <p class="m-0 p-0 text-center">X1</p>
+          <div class="form-group">
+          X1...<Input type='text' placeholder="0" size={3}>x</Input>
+          <Input  type='text' placeholder="0" size={3}>y</Input>
           </div>
-          <div class="col-md-3  m-1">
-            <Input type='text' value={0} size={3}>y</Input>
           </div>
-          <div class="col-md-3  m-1">
+          <div class="col-md-2 m-1">
           <select type='text' name='r1'>
             <option selected>....</option>
             <option> Mayor igual</option>
@@ -36,8 +35,8 @@ export const Home = () => {
             <option>Mayor</option>
           </select>
           </div>
-          <div class="col-md-3 m-1">
-          <Input type='text' value={0} size={3}>x+</Input>
+          <div class="col-md-2 m-1 ">
+          <Input type='text' placeholder="0" size={3}></Input>        
           </div>
         </div>
       </form>
