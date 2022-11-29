431f11e562de278183e795f6503e21fae45a26c0
abraham-cardenas <abrahamcar098@gmail.com>
2022-11-28 22:33:54 -0500

    actualizacion

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const calendarApi = axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'https://base-de-datos-empresa.herokuapp.com/'
 });
 
 //TODO: configurar interceptores
